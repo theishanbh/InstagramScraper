9251da27f46ce2c1df79c53c577d6b1e5c2c538d
theishanbh <91272949+theishanbh@users.noreply.github.com>
2021-12-14 19:14:14 +0530

    Add files via upload

---
+++ shopifymodels.py
@@ -0,0 +1,127 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException
+import time
+import os
+import datetime
+from webdriver_manager.chrome import ChromeDriverManager
+
+count = 700 # number of profiles you want to scrap
+account = ""  # account from
+page = "followers"  # from following or followers
+
+yourusername = ""  # your Instagram username
+yourpassword = ""  # your Instagram password
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument(
+    '--user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 12_1_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16D57"')
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get('https://www.instagram.com/accounts/login/')
+sleep(3)
+username_input = driver.find_element_by_css_selector("input[name='username']")
+password_input = driver.find_element_by_css_selector("input[name='password']")
+username_input.send_keys(yourusername)
+password_input.send_keys(yourpassword)
+login_button = driver.find_element_by_xpath("//button[@type='submit']")
+login_button.click()
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(.,'Not Now')]"))).click()
+sleep(3)
+
+driver.get('https://www.instagram.com/%s' % account)
+sleep(2)
+driver.find_element_by_xpath('//a[contains(@href, "%s")]' % page).click()
+scr2 = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+sleep(2)
+text1 = scr2.text
+print(text1)
+x = datetime.datetime.now()
+print(x)
+_list = []
+for i in range(1, count):
+    scr1 = driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/ul/div/li[%s]' % i)
+    driver.execute_script("arguments[0].scrollIntoView();", scr1)
+    sleep(1)
+    text = scr1.text
+    list = text.encode('utf-8').split()
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    csvfilename = os.path.join(dirname, account + "-" + page + ".txt")
+    file_exists = os.path.isfile(csvfilename)
+    f = open(csvfilename, 'a')
+    f.write(str(list[0]) + "\r\n")
+    f.close()
+    print('{};{}'.format(i, list[0]))
+    if i == (count - 1):
+        print(x)
+        _list.append(x)
+
+
+# import requests
+# import pandas as pd
+# from bs4 import BeautifulSoup
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# import os
+# from selenium.common.exceptions import TimeoutException
+# from selenium.webdriver.common.by import By
+# import selenium.webdriver.support.ui as ui
+# import selenium.webdriver.support.expected_conditions as EC
+# import time
+#
+# options = webdriver.ChromeOptions()
+# options.add_argument('--ignore-certificate-errors')
+# options.add_argument('--ignore-ssl-errors')
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# chromedriver = dir_path + "/chromedriver"
+# os.environ["webdriver.chrome.driver"] = chromedriver
+# driver = webdriver.Chrome(chrome_options=options, executable_path=chromedriver)
+
+# browser = Chrome()
+# url = "https://www.instagram.com/"
+# browser.get(url)
+
+
+#
+# page = requests.get("https://www.instagram.com/boutinela/following/?hl=en", headers={'User-Agent': 'Mozilla/5.0'})
+# soup = BeautifulSoup(page.content, "html.parser")
+# name = soup.find('h1').get_text()
+# print(soup)
+# name = soup.find(class_ = "FPmhX notranslate  _0imsa")
+# print(name)
+
+# start = soup.find('ul', class_="listGroup")
+# if start is None:
+#     continue
+# else:
+#     value = start.find_next('ul').contents
+#     # print("*")
+#     _string = ""
+#     for each in value:
+#         if each == "\n":
+#             continue
+#         else:
+#             _string += each.get_text() + ","
+#             # print("*")
+#     disease_symptoms.append(_string)
+#     disease_name.append(name)
+# print(i)
+
+#
+# excel = pd.DataFrame(
+#     {
+        # "Name": disease_name,
+        # "Symptoms": disease_symptoms
+
+    # })
+# excel.to_csv("DiseaseAndSymptom.csv")
